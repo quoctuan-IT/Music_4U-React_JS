bc6d6425f2efcf6016f93dd96a05e2070b0a5576
quoctuan-IT <phamquoctuan041203@gmail.com>
2026-05-26 11:58:11 +0700

    Set-up React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={< Home />}>
+          <Route path="/" element={< Home />} />
+          <Route path="/" element={< Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
